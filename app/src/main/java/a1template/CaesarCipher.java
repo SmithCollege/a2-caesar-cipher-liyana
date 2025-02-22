5609c09528779ea63540c53e0a4c5d6f2fb2290a
liyas1719 <lasariaissa@smith.edu>
2025-02-21 23:41:23 -0500

    updated 2/21 11pm

--- app/src/main/java/a1template/CaesarCipher.java
+++ app/src/main/java/a1template/CaesarCipher.java
@@ -6,9 +6,10 @@ public class CaesarCipher {
 
     /** Character array to store the letters in the alphabet in order */
     Character[] alphabet = new Character[26];
+    Character[] alphabet2 = new Character[26];
 
     /** DynamicArray object providing ArrayList-like operations for Characters */
-    DynamicArray<Character> cipher = new DynamicArray<Character>(26, alphabet);
+    DynamicArray<Character> cipher = new DynamicArray<Character>(26, alphabet2);
 
     /** Private offset that tracks how many positions to shift the index for
     * This cipher */
@@ -53,26 +54,57 @@ public class CaesarCipher {
         alphabet[23] = 'x';
         alphabet[24] = 'y';
         alphabet[25] = 'z';
-        //ystem.out.println(alphabet[20]);
         for (i=0; i<26; i++) {
-            if (offset <i) {
+            cipher.set(i, alphabet[(i + offset)%26]);
+            System.out.println(cipher.get(i));
+            //while (i<offset) {
+                //cipher.set(i, alphabet[26-offset+i]);
+               // System.out.println("alphabet" + alphabet[26-offset+i]);
+                //System.out.println(cipher.get(i)); 
+                //break;
+            //}
+            // while (i==offset) {
+            //     cipher.set(i, alphabet[0]);
+            //     System.out.println(cipher.get(i)); 
+            //     break;
+            // }
+            // while (i>=offset) {
+            //     cipher.set(i, alphabet[i-offset]);
+            //     System.out.println("i= " + i);
+            //     System.out.println("alph num= " + (i-offset));
+            //     System.out.println("alph num= " + alphabet[i-offset]);
+            //     System.out.println("alph n15= " + alphabet[15]);
+            //     System.out.println(cipher.get(i)); 
+            //     break;
+            // }
+            // if(i<offset-1) {
+            //     cipher.set(i, alphabet[26-offset+i]);
+            //     System.out.println(cipher.get(i));
+            // } //else if (i == offset) {
+            //     //cipher.set(i, alphabet[0]); 
+            //     //System.out.println(cipher.get(i)); 
+            //  else if (i>offset) {
+            //     cipher.set(i, alphabet[i-offset]);
+            //     System.out.println(cipher.get(i));
+            // }
+            //if ( {
                 //System.out.println("Complete " + i + alphabet[i]);
                 //cipher.set((i+offset), alphabet[i]);
                 //cipher.set((i-offset+1), alphabet[i]);
-                cipher.set(i, alphabet[i-offset+1]);
-                System.out.println("Complete " + i + alphabet[i] + (i-offset) + cipher.get(i));
+                // cipher.set((i-offset-1), alphabet[i-offset]);
+                // System.out.println("Complete " + i + alphabet[i-offset] + (i-offset-1) + cipher.get(i));
                // System.out.println("Complete " + cipher.get(i) + alphabet[i] +i);
-            } else {
+            //} else {
                 //System.out.println("Complete " + i + alphabet[i]);
                 //System.out.println("Complete " + i + " more than");
                 //cipher.set((i+25-offset), alphabet[i]);
-                cipher.set((i), alphabet[i+25-offset]);
-                System.out.println("Complete " + i + alphabet[i] + (i+25-offset) + cipher.get(i));
+                // cipher.set(i, alphabet[((25+i-offset))]);
+                // System.out.println("Complete " + i + alphabet[25+i-offset] + ((26+i-offset)) + cipher.get(i));
                 //System.out.println("Complete " + cipher.get(i) + alphabet[i] + i);
             }
         }
 
-    }
+    
 
     public int get(int i) {
         System.out.println(cipher.get(i));
