f9c25e73ef8ec6df10a204870290ea0960dd2fca
liyas1719 <lasariaissa@smith.edu>
2025-02-21 20:26:52 -0500

    update 2/21

--- app/src/main/java/a1template/CaesarCipher.java
+++ app/src/main/java/a1template/CaesarCipher.java
@@ -5,21 +5,73 @@ package a1template;
 public class CaesarCipher {
 
     /** Character array to store the letters in the alphabet in order */
-    Character[] alphabet;
+    Character[] alphabet = new Character[26];
 
     /** DynamicArray object providing ArrayList-like operations for Characters */
-    DynamicArray<Character> cipher;
+    DynamicArray<Character> cipher = new DynamicArray<Character>(26, alphabet);
 
     /** Private offset that tracks how many positions to shift the index for
     * This cipher */
-    private int offset;
+    public int i;
+    public int k;
+
+    public CaesarCipher(Character[] alphabet, DynamicArray<Character> cipher, int i, int k) {
+        this.alphabet = alphabet;
+        this.cipher = cipher;
+        this.i = i;
+        this.k = k;
+    }
 
     /** Constructor that should define the instance variables, including
      * populating the alphabet
      * @param offset Offset to use when creating `cipher` of DynamicArray type
      */
     CaesarCipher(int offset){
-        // Fill in here
+        alphabet[0] = 'a';
+        alphabet[1] = 'b';
+        alphabet[2] = 'c';
+        alphabet[3] = 'd';
+        alphabet[4] = 'e';
+        alphabet[5] = 'f';
+        alphabet[6] = 'g';
+        alphabet[7] = 'h';
+        alphabet[8] = 'i';
+        alphabet[9] = 'j';
+        alphabet[10] = 'k';
+        alphabet[11] = 'l';
+        alphabet[12] = 'm';
+        alphabet[13] = 'n';
+        alphabet[14] = 'o';
+        alphabet[15] = 'p';
+        alphabet[16] = 'q';
+        alphabet[17] = 'r';
+        alphabet[18] = 's';
+        alphabet[19] = 't';
+        alphabet[20] = 'u';
+        alphabet[21] = 'v';
+        alphabet[22] = 'w';
+        alphabet[23] = 'x';
+        alphabet[24] = 'y';
+        alphabet[25] = 'z';
+        //ystem.out.println(alphabet[20]);
+        for (i=0; i<26; i++) {
+            if (i < 25-offset) {
+                System.out.println("Complete " + i + alphabet[i]);
+                //cipher.set((i+offset), alphabet[i]);
+               // System.out.println("Complete " + cipher.get(i) + alphabet[i] +i);
+            } else {
+                System.out.println("Complete " + i + alphabet[i]);
+                //System.out.println("Complete " + i + " more than");
+                //cipher.set((i-25+offset), alphabet[i]);
+                //System.out.println("Complete " + cipher.get(i) + alphabet[i] + i);
+            }
+        }
+
+    }
+
+    public int get(int i) {
+        return cipher.get(i); 
+        //return alphabet[i];
     }
 
     /** Implementation of linear search that looks through the alphabet
@@ -28,16 +80,26 @@ public class CaesarCipher {
      * @return int indicating position of val in the alphabet array
      */
     public int findIndex(char val){
-        // This is a stub -- fill in the code and return the
-        // value you calculate
-        return 0;
+        for (i=0; i<26; i++) {
+            if (val == alphabet[i]) {
+                k = i; 
+            }
+        }
+        return k;
     }
 
     /** Encode a message using the cipher
      * @param T message to encode
      * @return encoded message */  
     public String encode(String message){
-        // Fill in here and update return statement based on your code
+        char[] charArray = message.toCharArray();
+        for(i=0; i< charArray.length; i++) {
+            if () {
+
+            // }
+
+        }
+
         return new String(); 
      }
 
@@ -52,6 +114,7 @@ public class CaesarCipher {
     }
 
     public static void main(String[] args) {
+        CaesarCipher classUnderTest = new CaesarCipher(3);
     }
     
 }
--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -4,7 +4,40 @@ package a1template;
 
 public class DynamicArray<T> implements IndexAccess<T>{
     
-    // What instance variables do you need?
+    public int i;
+    public T[] array;
+    // DynamicArray<T> dynamicArray = new DynamicArray<T>(i, val);
+
+    public DynamicArray(int i, T[] val) {
+        this.i = i;
+        this.array = val;
+    }
+
+    /** Returns the value stored at a given index
+     * @param i index of element to read
+     * @return value stored at the given index
+     */
+    public T get(int i) {
+        return array[i]; 
+    }
+
+    /** Returns the value stored at a given index plus the offset
+     * @param i index of element to read
+     * @param offset adjust index by this value
+     * @return value stored at the given index
+     */
+    public T get(int i, int offset) {
+        return array[i+offset];
+    }
+
+
+    /** Stores the given value at the given index
+     * @param i index of locaßtion to store
+     * @param val value to store at given index
+     */
+    public void set(int i, T val) {
+        array[i] = val;
+    }
 
     // Write a constructor to make a new DynamicArray Object from an array
 
